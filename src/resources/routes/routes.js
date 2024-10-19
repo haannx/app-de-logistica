345e7f63d3adf08e818aab79fa0833dc34264d55
haannx <viniciosbohnqwe@outlook.com>
2024-10-18 23:25:37 -0300

    1.0.4 consulta de clientes

--- src/resources/routes/routes.js
+++ src/resources/routes/routes.js
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import PesquisaPedidos from '../sidebar/sidebaritens/pedidos/consultadepedidos/pedidos'
 import Home from './Home'
 import ConsultaClientes from '../sidebar/sidebaritens/clientes/consultadeclientes/index'
-import PesquisaProdutos from '../sidebar/sidebaritens/estoque/consultadeprodutos'
+import ConsultaProdutos from '../sidebar/sidebaritens/estoque/consultadeprodutos'
 import CadastroClientes from '../sidebar/sidebaritens/clientes/cadastrodeclientes'
 import CadastroProdutos from '../sidebar/sidebaritens/estoque/cadastrodeprodutos'
 
@@ -12,7 +12,7 @@ const AppRoutes = () => (
     <Route path='/' element={<Home />} />
     <Route path='/consultarpedidos' element={<PesquisaPedidos/>}/>
     <Route path='/consultarclientes' element={<ConsultaClientes/>}/>
-    <Route path='/consultarprodutos' element={<PesquisaProdutos/>}/>
+    <Route path='/consultarprodutos' element={<ConsultaProdutos/>}/>
     <Route path='/cadastroprodutos' element={<CadastroProdutos/>}/>
     <Route path='/cadastrarclientes' element={<CadastroClientes/>}/>
   </Routes>
--- src/resources/sidebar/sidebaritens/estoque/consultadeprodutos/index.js
+++ src/resources/sidebar/sidebaritens/estoque/consultadeprodutos/index.js
@@ -1,73 +1,152 @@
-import styled from "styled-components"
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 
-const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
+const ContainerGeral = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    overflow-y: scroll;
+    padding-bottom: 20px;
 `
-const ContainerPesquisa = styled.div`
-    display: flex;
-    margin-bottom: 20px;
-    margin-right: 150px;
+const Container = styled.div`
+    max-width: 1000px;
+    margin: 20px;
+    padding: 20px;
+    margin-left: 50px;
 `
-const Descricao = styled.label`
+const TituloPrincipal = styled.h1`
     color: #fff;
-    font-family: 'Calibri', 'Cambria', sans-serif;
-    font-weight: bold;
-    margin-right: 10px; // Espaço entre o label e o input
-    width: 150px; // Largura fixa para os labels
-    text-align: right; // Alinha o texto à direita para ficar próximo ao input
+    margin-top: 60px;
+    margin-left: 60px;
 `
-const BarradePesquisa = styled.input`
-    width: 400px;
-    height: 25px;
+const InputPesquisa = styled.input`
+    padding: 8px;
+    margin-bottom: 20px;
+    width: 600px;
+    border-radius: 4px;
     border: 1px solid #ccc;
-    border-radius: 7px;
-    color: #fff;
-    background-color: #000;
-
 `
-const Botao = styled.button`
-    padding: 7px 10px;
-    background-color: #4a90e2;
-    color: #fff;
-    border: none;
-    border-radius: 7px;
-    cursor: pointer;
-
-    &:hover {
-        background-color: #357ab7;
-    }
-`
-const Titulo = styled.h1`
-    color: #fff;
-    font-family: 'Calibri', 'Cambria', sans-serif;
-    font-weight: bold;
+const CheckboxContainer = styled.div`
     margin-bottom: 20px;
-`
-const Resultadopedidos = styled.h2`
     color: #fff;
-    margin: 30px;
+`
+const CheckboxLabel = styled.label`
+    margin-right: 10px;
+`
+const Tabela = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+`
+const Th = styled.th`
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+    border: 1px solid #ccc;
+`
+const Td = styled.td`
+    padding: 10px;
+    border: 1px solid #ccc;
+    color: #fff;
+`
+const CheckBox = styled.input`
+    margin: 10px;
 `
 
-function PesquisaProdutos() {
+function ConsultaProdutos() {
+  const [produtos, setProdutos] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [searchById, setSearchById] = useState(false)  // Alterado para false
+  const [searchByDescricao, setSearchByDescricao] = useState(false)  // Alterado para false
+  const [searchByMarca, setSearchByMarca] = useState(false)  // Alterado para false
+
+  useEffect(() => {
+    const produtosSalvos = JSON.parse(localStorage.getItem('produtos')) || []
+    setProdutos(produtosSalvos)
+  }, [])
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  const produtosFiltrados = produtos.filter((produto) => {
+    const searchTermLower = searchTerm.toLowerCase()
+
     return (
-        <Container>
-            <Titulo>Consulta de Produtos</Titulo>
-            <ContainerPesquisa>
-                <Descricao>Número do Produto</Descricao>
-                <BarradePesquisa />
-            </ContainerPesquisa>
-            <ContainerPesquisa>
-                <Descricao>Descrição do Produto</Descricao>
-                <BarradePesquisa />
-            </ContainerPesquisa>
-            <Botao>Pesquisar</Botao>
-            <Resultadopedidos>Produtos</Resultadopedidos>
-        </Container>
+      (searchById && produto.id.toString().includes(searchTerm)) ||
+      (searchByDescricao && produto.descricao.toLowerCase().includes(searchTermLower)) ||
+      (searchByMarca && produto.marca.toLowerCase().includes(searchTermLower))
     )
+  })
+
+  return (
+    <ContainerGeral>
+      <TituloPrincipal>Consulta de Produtos</TituloPrincipal>
+      <Container>
+        <InputPesquisa
+          type="text"
+          placeholder="Pesquise por ID, descrição ou marca..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+
+        <CheckboxContainer>
+          <CheckboxLabel>
+            <CheckBox
+              type="checkbox"
+              checked={searchById}
+              onChange={(e) => setSearchById(e.target.checked)}
+            /> 
+            ID
+          </CheckboxLabel>
+          <CheckboxLabel>
+            <CheckBox
+              type="checkbox"
+              checked={searchByDescricao}
+              onChange={(e) => setSearchByDescricao(e.target.checked)}
+            /> 
+            Descrição
+          </CheckboxLabel>
+          <CheckboxLabel>
+            <CheckBox
+              type="checkbox"
+              checked={searchByMarca}
+              onChange={(e) => setSearchByMarca(e.target.checked)}
+            /> 
+            Marca
+          </CheckboxLabel>
+        </CheckboxContainer>
+
+        <Tabela>
+          <thead>
+            <tr>
+              <Th>ID</Th>
+              <Th>Descrição</Th>
+              <Th>Marca</Th>
+            </tr>
+          </thead>
+          <tbody>
+            {produtosFiltrados.length > 0 ? (
+              produtosFiltrados.map((produto) => (
+                <tr key={produto.id}>
+                  <Td>{produto.id}</Td>
+                  <Td>{produto.descricao}</Td>
+                  <Td>{produto.marca}</Td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <Td colSpan="3">Nenhum produto encontrado</Td>
+              </tr>
+            )}
+          </tbody>
+        </Tabela>
+      </Container>
+    </ContainerGeral>
+  )
 }
 
-export default PesquisaProdutos
+export default ConsultaProdutos
+
+
